88b2c66eaead43dfb6bea9eab8e3e5db7bbe1fd0
Jeroen Kesteloo <jeroen.kesteloo@finaps.nl>
2017-03-01 14:57:39 +0100

    Bij foutief inloggen wordt het wachtwoord veld leeggemaakt zodat in IE het bekijken van het wachtwoord weer werkt

--- src/LoginForm/widget/LoginForm.js
+++ src/LoginForm/widget/LoginForm.js
@@ -272,6 +272,7 @@ define([
             this._logineventbusy = false;
             this.passwordInputNode.disabled = false;
             this.usernameInputNode.disabled = false;
+            this.passwordInputNode.value = '';
         },
         /**
          * Retrieves the matching value from the internationalization object
